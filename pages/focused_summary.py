d4320fce0529786090958b4ac59183b6c4b4607f
Priscilla Mukudan <priscilla.mukudan@gmail.com>
2023-12-03 22:12:53 +0800

    Correct indentation in focused_summary to ensure generate prompt button appears correctly.

--- pages/focused_summary.py
+++ pages/focused_summary.py
@@ -12,9 +12,8 @@ with initial_inputs:
     summary_length = st.number_input("Maximum number of words:", min_value=1, max_value=1000, value=get_default_value('summary_length', 50), step=1, format='%d')
     summary_focus = st.text_input("Focus of the summary:", get_default_value('summary_focus', ''))
     final_input = st.session_state['initial_input'] + f'\nMaximum number of words: {summary_length}\nFocus of the summary: {summary_focus}'
-
-inputs_to_save = {"summary_length":summary_length, "summary_focus":summary_focus, 'final_input':final_input, "prompt_page":"focused_summary"}
-completed_input(inputs_to_save, None, 'inital', 'Generate prompt')
+    inputs_to_save = {"summary_length":summary_length, "summary_focus":summary_focus, 'final_input':final_input, "prompt_page":"focused_summary"}
+    completed_input(inputs_to_save, None, 'inital', 'Generate prompt')
 
 with selections:
     st.subheader("Selection")
